a23770275f1f2aa46df16d9c79e6e7a00d5e0564
Raffaele Grieco <62102593+Baraff24@users.noreply.github.com>
2024-10-08 12:42:00 +0200

    Change cis max_length from 20 to 60

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -45,7 +45,7 @@ class Structure(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     address = models.CharField(max_length=200)
-    cis = models.CharField(max_length=20, unique=True)
+    cis = models.CharField(max_length=60, unique=True)
 
     def __str__(self):
         return str(self.name)
